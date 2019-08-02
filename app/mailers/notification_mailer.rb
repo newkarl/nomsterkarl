33bd96f76b65d3d9f1939599ea48923355ad4e57
karl newton <newkarl@gmail.com>
2019-08-01 20:11:27 -0500

    nomster43

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+  def comment_added(comment)
+  @place = comment.place
+  @place_owner = @place.user
+  mail(to: "newkarl@gmail.com",
+       subject: "A comment has been added to your place")
+  end
 end
